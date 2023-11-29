1a9798737e5ad1cbc6bca3538009d5215ad20c72
Wojciech Jarosz <jaroszw@gmail.com>
2023-11-29 12:47:38 +0100

    adding user route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js';
 import 'dotenv/config';
+
+console.log('FRESH');
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,3 +19,5 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server is listenning on port 3000');
 });
+
+app.use('/api/user', userRoutes);
