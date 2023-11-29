da0b28dcf782d8bcb3907c20f304376d9edd54bc
Wojciech Jarosz <jaroszw@gmail.com>
2023-11-29 14:33:46 +0100

    Add middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+// import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
 
@@ -9,8 +10,6 @@ export const signup = async (req, res) => {
   try {
     await newUser.save();
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
-
-  res.status(201).json({ message: 'user created sucesfully' });
 };
